0854320a54992c5bd5a9e9c4a440137936b17a68
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-03-20 12:00:52 -0300

    Conclusão do desafio 01

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -16,12 +16,13 @@ interface TasksListProps {
   tasks: Task[];
   toggleTaskDone: (id: number) => void;
   removeTask: (id: number) => void;
+  //editTask: (taskId: number, taskNewTitle: string) => void;
 }
 
 export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
   return (
     <FlatList
-      // data={tasks}
+      data={tasks}
       keyExtractor={item => String(item.id)}
       contentContainerStyle={{ paddingBottom: 24 }}
       showsVerticalScrollIndicator={false}
@@ -33,11 +34,13 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
+                onPress={() => toggleTaskDone(item.id)}
+                
               >
                 <View 
                   testID={`marker-${index}`}
-                  //TODO - use style prop 
+                 
+                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
                 >
                   { item.done && (
                     <Icon 
@@ -49,7 +52,8 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 </View>
 
                 <Text 
-                  //TODO - use style prop
+                  
+                  style={item.done ? styles.taskTextDone : styles.taskText}
                 >
                   {item.title}
                 </Text>
@@ -59,7 +63,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
             <TouchableOpacity
               testID={`trash-${index}`}
               style={{ paddingHorizontal: 24 }}
-              //TODO - use onPress (remove task) prop
+              onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
             </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,72 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
+
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const duplicateTask = tasks.find(task => task.title === newTaskTitle);
+
+    if(duplicateTask){
+      return Alert.alert("Task já cadastrada","Você não pode cadastrar uma task com o mesmo nome")
+    }
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({...task}))
+    
+    const task = updatedTasks.find(task => task.id === id)
+
+    if(!task){
+      return
+    }
+    task.done = !task.done;
+
+    setTasks(updatedTasks)
   }
 
+  function handleEditTask(taskId: number, taskNewTitle: string){
+    const updatedTasks = tasks.map(task => ({...task}))
+    
+    const task = updatedTasks.find(task => task.id === taskId)
+
+    if(!task){
+      return
+    }
+    task.title = taskNewTitle;
+
+    setTasks(updatedTasks)
+  }
+  
+
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    // Alert.alert('Remover item', 'Tem certeza que você deseja remover esse item?',
+    //   [
+    //     {
+    //       text: "Não",
+    //       onPress:()=> console.log()
+    //     },
+       
+    //     {
+    //       text: "Sim",
+    //       onPress:()=> setTasks(oldTasks => oldTasks.filter(task => task.id !== id))
+    //     }
+    // ])
+    
+    setTasks(oldTasks => oldTasks.filter(task => task.id !== id))
   }
 
   return (
@@ -30,6 +79,7 @@ export function Home() {
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
+        //editTask={handleEditTask}
       />
     </View>
   )
